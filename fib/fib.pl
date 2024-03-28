9e3c57b67efb96c9f9543411988f89d9ff1bc3d1
Nicholas J. Kain <nicholas@kain.us>
2024-03-28 17:15:22 -0400

    fib.pl: Shrink, also seems very slightly faster.

--- fib/fib.pl
+++ fib/fib.pl
@@ -1,7 +1,6 @@
 #!/usr/bin/env perl
 sub fib {
     my $n = shift;
-    return $n if $n < 2;
-    return fib($n - 1) + fib($n - 2);
+    return $n < 2 ? $n : fib($n - 1) + fib($n - 2);
 }
 printf("%d\n", fib(18));
