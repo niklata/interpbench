f56b8db4bdae58b390ff76fedbedec13870b66e3
Nicholas J. Kain <nicholas@kain.us>
2024-05-08 00:17:25 -0400

    Yet more perl golf.

--- sieve/sieve.pl
+++ sieve/sieve.pl
@@ -1,22 +1,16 @@
 #!/usr/bin/env perl
 sub sieve_sundaram {
-    my $n = shift;
+    my ($n,@s) = (shift, (2));
     my %a;
-    my @s = ( 2 );
-    my $m = $n / 2;
     for my $i (1..($n-1)) {
         for my $j ($i..($n-1)) {
             my $p = $i + $j + 2 * $i * $j;
             $a{$p} = 1 if $p <= $n;
         }
     }
-    for my $k (1..$m) {
-        push @s, 2 * $k + 1 if !$a{$k};
-    }
+    map { push @s, 2 * $_ + 1 if !$a{$_}; } 1..($n/2);
     return @s;
 }
 
-for my $i (sieve_sundaram(1000)) {
-    print $i, " ";
-}
-print("\n");
+map { print $_, " "; } sieve_sundaram(1000);
+print "\n";
